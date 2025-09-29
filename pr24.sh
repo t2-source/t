d872d9bc09935090df8ffb32489c7608be22e0fc
t2-source <t2@40.aa01.my03.com>
2025-09-29 20:41:57 +0800

    Update pr24.sh

--- pr24.sh
+++ pr24.sh
@@ -4,7 +4,8 @@ set -e
 echo "[*] Install base dependencies with nix-env..."
 
 # install tools & bahasa
-nix-env -iA nixpkgs.wget nixpkgs.curl nixpkgs.unzip nixpkgs.git nixpkgs.python3 nixpkgs.nodejs_20 nixpkgs.systemd
+nix-env -iA nixpkgs.wget nixpkgs.curl nixpkgs.unzip nixpkgs.git nixpkgs.python3 nixpkgs.nodejs_20 nixpkgs.systemd nixpkgs.python3Packages.playwright
+
 
 echo "[*] Init Node.js project..."
 npm init -y
